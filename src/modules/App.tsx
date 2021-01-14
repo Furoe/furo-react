267c4159bb4e26e4e9b08187927432f243e61bad
furoe <17751038730@163.com>
2021-01-14 20:36:21 +0800

    添加react-router

--- src/modules/App.tsx
+++ src/modules/App.tsx
@@ -1,16 +1,13 @@
 import React, { FC } from 'react';
 import logo from '../assets/styles/logo.svg';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import '../assets/styles/App.scss';
 import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  ShopOutlined,
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
   TeamOutlined,
   UserOutlined,
-  UploadOutlined,
-  VideoCameraOutlined,
 } from '@ant-design/icons';
 //import { Header } from 'antd/lib/layout/layout';
 const { Header, Sider, Content, Footer} = Layout;
@@ -18,146 +15,24 @@ const { SubMenu } = Menu;
 
 const App: FC = () => (
   <div className="App">
-      <Layout>
-        <Sider 
-          style={{
-            overflow: 'auto',
-            height: '100vh',
-            position: 'fixed',
-            left: 0,
-          }}
-        >
+      <Layout className="layout" style={{ minHeight: '100vh'}}>
+        <Header>
           <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-            <Menu.Item key="1" icon={<UserOutlined />}>
-              nav 1
-            </Menu.Item>
-            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-              nav 2
-            </Menu.Item>
-            <Menu.Item key="3" icon={<UploadOutlined />}>
-              nav 3
-            </Menu.Item>
-            <Menu.Item key="4" icon={<BarChartOutlined />}>
-              nav 4
-            </Menu.Item>
-            <Menu.Item key="5" icon={<CloudOutlined />}>
-              nav 5
-            </Menu.Item>
-            <Menu.Item key="6" icon={<AppstoreOutlined />}>
-              nav 6
-            </Menu.Item>
-            <Menu.Item key="7" icon={<TeamOutlined />}>
-              nav 7
-            </Menu.Item>
-            <Menu.Item key="8" icon={<ShopOutlined />}>
-              nav 8
-            </Menu.Item>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
           </Menu>
-        </Sider>
-        <Layout className="site-layout" style={{ marginLeft: 200 }}>
-          <Header className="site-layout-background" style={{ padding: 0 }} />
-          <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-            <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
-              ...
-              <br />
-              Really
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              long
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              ...
-              <br />
-              content
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>©2021 Created by Furo_Yang</Footer>
-        </Layout>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content" style={{minHeight: '70vh' }}>Content</div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>©2021 Created by Furo</Footer>
       </Layout>
     </div>
 );
