60c23b6e3103f2060bb7ec6bf2eac88472c575e4
furoe <17751038730@163.com>
2021-01-15 22:45:35 +0800

    添加ts下router依赖

--- src/modules/App.tsx
+++ src/modules/App.tsx
@@ -18,10 +18,10 @@ const App: FC = () => (
       <Layout className="layout" style={{ minHeight: '100vh'}}>
         <Header>
           <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">主页</Menu.Item>
+            <Menu.Item key="2">归档</Menu.Item>
+            <Menu.Item key="3">工具箱</Menu.Item>
           </Menu>
         </Header>
         <Content style={{ padding: '0 50px' }}>
@@ -30,7 +30,7 @@ const App: FC = () => (
             <Breadcrumb.Item>List</Breadcrumb.Item>
             <Breadcrumb.Item>App</Breadcrumb.Item>
           </Breadcrumb>
-          <div className="site-layout-content" style={{minHeight: '70vh' }}>Content</div>
+          <div className="site-layout-content" style={{minHeight: '72vh' }}>Content</div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>©2021 Created by Furo</Footer>
       </Layout>
